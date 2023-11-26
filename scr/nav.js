34e95ae9bf05baa7ee3ffa1ee09963a2387e864c
Bohdan <bohdan.g.54@gmail.com>
2023-11-26 03:30:56 +0200

    header is done completely

--- scr/nav.js
+++ scr/nav.js
@@ -1,23 +1,50 @@
+let wasActionBefore = true;
+let lastScrollTop = 0;
+
 window.onscroll = function() {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
     let navbar = document.querySelector('.navbar');
     let navbarBrand = document.querySelector('.navbar-brand');
+    let layersTitle = document.querySelector('.layers__title');
+    let layersMiddle = document.querySelector('.layers__middle');
 
-    console.log(scrollTop);
+        // layersMiddle.style.opacity = "100";
+        // let num = scrollTop - 300;
+        // if (num > 100) {
+        //     num = 100;
+        // }
+        // console.log(num.toString());
+        // layersMiddle.style.opacity = num.toString();
 
     if (scrollTop < 600) {
         navbar.classList.add('scrolled')
         navbarBrand.classList.add('scrolled');
+        let currentOpacity = parseFloat(window.getComputedStyle(layersMiddle).opacity);
+        let currentFontSize = parseFloat(window.getComputedStyle(layersTitle).fontSize);
 
-        console.log('User is at the top of the page');
+        if (scrollTop > lastScrollTop) {
+            wasActionBefore = true;
+            currentOpacity = 0;
+            currentFontSize += 0.1;
 
+        } else if (wasActionBefore) {
+            // console.log(currentFontSize);
+            currentOpacity = 1;
+            currentFontSize -= 0.1;
+        }
+        console.log(currentOpacity);
+        layersMiddle.style.opacity = currentOpacity.toString();
+        layersTitle.style.fontSize = currentFontSize + 'px';
     } else {
         var classList = navbar.classList;
         if (classList.contains('scrolled')) {
             navbarBrand.classList.remove('scrolled')
             classList.remove('scrolled')
         }
-        console.log('User has scrolled down');
+        layersMiddle.style.opacity = "1";
+        layersTitle.style = "";
+        wasActionBefore = false;
     }
+    lastScrollTop = scrollTop;
 };
 
